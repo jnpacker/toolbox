db10b04a2f462efa6a754e897ced243e1753ce0e
Joshua Packer <jnp.mobile@gmail.com>
2020-07-22 00:42:47 -0400

    Slight adjustments to the docker run command script

--- pihole/run-pihole-container.sh
+++ pihole/run-pihole-container.sh
@@ -1,5 +1,5 @@
-docker ps pihole | grep pihole > /dev/null 2>&1
+docker ps | grep pihole > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     docker rm -f pihole
 fi
-docker run -p 80:80 -p 53:53/tcp -p 53:53/udp -e ServerIP=192.168.0.26 -e TZ=CA -e DNS1=1.1.1.1 -eDNS2=8.8.8.8 -e DNSSEC=true --name pihole -v "$(pwd)/pihole/etc/:/etc/pihole/" -v "$(pwd)/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" --restart=unless-stopped -d pihole/pihole:latest
+docker run --net=bridge -e ServerIP=192.168.0.26 -p 80:80 -p 53:53/tcp -p 53:53/udp -e ServerIP=192.168.0.26 -e TZ=CA -e DNS1=1.1.1.1 -e DNS2=8.8.8.8 --dns=127.0.0.1 --dns=1.1.1.1 -e DNSSEC=true --name pihole -v "$(pwd)/pihole/etc/:/etc/pihole/" -v "$(pwd)/pihole/etc-dnsmasq.d/:/etc/dnsmasq.d/" --restart=unless-stopped -d pihole/pihole:latest
